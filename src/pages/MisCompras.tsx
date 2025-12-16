674b208223be5645f84988e0669fcbbd1150b3a3
Leonardo Diaz <diazleonardo237@gmail.com>
2025-12-16 09:54:53 -0500

    Remplaza el contenido de la compra por un componente

--- src/pages/MisCompras.tsx
+++ src/pages/MisCompras.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getComprasByUser, type Compra } from "../services/compras"
 import OverlayLoader from "../components/common/OverlayLoader"
+import CompraCard from "../components/misCompras/CompraCard"
 
 const MisCompras = () => {
     const [compras, setCompras] = useState<Compra[] | null>(null)
@@ -45,36 +46,7 @@ const MisCompras = () => {
 
                         <div className="flex flex-col gap-4">
                             {compra.items.map((item) => (
-                                <div
-                                    key={item.variant.id}
-                                    className="flex gap-4 p-3 bg-gray-50 rounded-lg"
-                                >
-                                    <img
-                                        src={`${import.meta.env.VITE_BASE_URL}storage/${item.article.imagen}`}
-                                        alt={item.article.nombre}
-                                        className="w-24 h-24 object-contain rounded-md bg-white"
-                                    />
-
-                                    <div className="flex flex-col justify-between flex-1">
-                                        <div>
-                                            <p className="font-medium">{item.article.nombre}</p>
-
-                                            {(item.variant.color || item.variant.size) && (
-                                                <p className="text-xs text-gray-500">
-                                                    {item.variant.color && `Color: ${item.variant.color}`}
-                                                    {item.variant.color && item.variant.size && " | "}
-                                                    {item.variant.size && `Talla: ${item.variant.size}`}
-                                                </p>
-                                            )}
-                                            <p className="text-sm text-gray-600 mt-1">
-                                                {item.cantidad} × S/ {item.precio.toFixed(2)}
-                                            </p>
-                                        </div>
-                                        <p className="font-semibold text-[#B8860B] mt-2">
-                                            Subtotal: S/ {item.subtotal.toFixed(2)}
-                                        </p>
-                                    </div>
-                                </div>
+                                <CompraCard item={item} />
                             ))}
                         </div>
                     </div>
